e0d9cfa32f98dd2130ab3e854b32adbd812fba82
Stephen Reid <stephen@stephenreid.net>
2025-08-25 10:52:58 +0200

    🍱 Add Hotwire Native support assets and configuration endpoints
    Introduce native.css to hide/show elements when running inside
    Hotwire Native apps. Add ConfigurationsController serving iOS and
    Android JSON rules that define modal and map view behaviors.

---
+++ rails/app/controllers/configurations_controller.rb
@@ -0,0 +1,62 @@
+class ConfigurationsController < ApplicationController
+  def ios_v1
+    render json: {
+      settings: {},
+      rules: [
+        {
+          patterns: [
+            "/new$",
+            "/edit$"
+          ],
+          properties: {
+            context: "modal"
+          }
+        },
+        {
+          patterns: [
+            "/events/[0-9]+/map"
+          ],
+          properties: {
+            view_controller: "map"
+          }
+        }
+      ]
+    }
+  end
+
+  def android_v1
+    render json: {
+      settings: {},
+      rules: [
+        {
+          patterns: [
+            ".*"
+          ],
+          properties: {
+            uri: "hotwire://fragment/web",
+            pull_to_refresh_enabled: true
+          }
+        },
+        {
+          patterns: [
+            "/new$",
+            "/edit$"
+          ],
+          properties: {
+            context: "modal",
+            pull_to_refresh_enabled: false
+          }
+        },
+        {
+          patterns: [
+            "/events/[0-9]+/map"
+          ],
+          properties: {
+            uri: "hotwire://fragment/map",
+            title: "Map"
+          }
+        }
+      ]
+    }
+  end
+end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Read-only routes for events
   resources :events, only: [:index, :show]
 
+  resources :configurations, only: [] do
+    get :ios_v1, on: :collection
+    get :android_v1, on: :collection
+  end
+
   root "events#index"
 end
